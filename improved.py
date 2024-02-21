276be2b97e5bddc313f08046eba72ea8abbb50bd
Kamran Khan Alwi <beingkamrankhanalwi@gmail.com>
2024-02-21 13:43:43 +0530

    update

--- improved.py
+++ improved.py
@@ -4,13 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementClickInterceptedException
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 from datetime import datetime
-import pandas as pd
 import csv
 import os
 from io import BytesIO
